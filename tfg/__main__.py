ae4e28ffc6a5380ea7940a52e4333d2d19cce44a
David Afonso <davafons@gmail.com>
2019-11-08 14:24:48 +0000

    :art: Add missing __main__ sentence.
    Also, add vim config oneliner and python shebang

--- tfg/__main__.py
+++ tfg/__main__.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python3
+# vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
+
+"""
+Entry point for the application.
+"""
+
 import sys
 
 from PySide2.QtWidgets import QApplication, QLabel
 
-app = QApplication(sys.argv)
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
 
-label = QLabel("<font color=Red size=40>Hello World!</font>")
-label.show()
-app.exec_()
+    label = QLabel("<font color=Red size=40>Hello World!</font>")
+    label.show()
+    app.exec_()
