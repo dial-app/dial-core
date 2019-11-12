0d59f9e2cdc4977fba52bf85689da6876e6c18f4
David Afonso <davafons@gmail.com>
2019-11-12 20:03:54 +0000

    :wrench: Add __init__.py to module
    Contains metadata information for the package

---
+++ tfg/__init__.py
@@ -0,0 +1,7 @@
+"""A Deep Learning GUI-based Framework"""
+
+__author__ = "David Afonso"
+__license__ = "GPL"
+__version__ = "0.0.0"
+__version_info__ = tuple(int(part) for part in __version__.split("."))
+__description__ = "A Deep Learning GUI-based Framework"
