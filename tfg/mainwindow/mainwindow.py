32dd1ff252602c9bea05d922ed88cb1aa1593b21
David Afonso <davafons@gmail.com>
2019-11-15 13:44:32 +0000

    :sparkles: Add tabbed widget to start attaching elements to the UI

--- tfg/mainwindow/mainwindow.py
+++ tfg/mainwindow/mainwindow.py
@@ -2,7 +2,8 @@
 
 """The main window for the program."""
 
-from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtCore import QSize
+from PySide2.QtWidgets import QApplication, QMainWindow, QTabWidget, QWidget
 
 from tfg.utils import log
 
@@ -18,19 +19,29 @@ class MainWindow(QMainWindow):
         super().__init__(parent)
 
         self._main_menu_bar = MainMenuBar(self)
+        self._tabs_widget = QTabWidget(self)
 
         self._setup()
 
         log.mainwindow.info("Program Initialized")
 
     def _setup(self):
+        # Widget configuration
         self.setWindowTitle("TFG")
 
         self.setMenuBar(self._main_menu_bar)
 
+        self.setCentralWidget(self._tabs_widget)
+
+        # Tab widget
+        self._tabs_widget.addTab(QWidget(self), "Datasets")
+
         # Connections
         self._main_menu_bar.quit.connect(QApplication.quit)
         self._main_menu_bar.toggle_log_window.connect(self._toggle_log_window)
+
+    def sizeHint(self):
+        return QSize(800, 600)
 
     def _toggle_log_window(self):
         dialog = log.LoggerTextboxDialog(self)
