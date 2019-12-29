7bc7b3b20732ed46db1c97bbf76c41aca62014a8
David Afonso <davafons@gmail.com>
2019-12-29 22:29:34 +0000

    :bug: Using Qt.UserRole to avoid collisions with other Roles

--- dial/utils/enums.py
+++ dial/utils/enums.py
@@ -1,6 +1,8 @@
 from enum import IntEnum
 
+from PySide2.QtCore import Qt
+
 
 class Dial(IntEnum):
-    RawRole = 15
-    TypeRole = 16
+    RawRole = Qt.UserRole  # The data in a raw format (as it is)
+    TypeRole = Qt.UserRole + 1  # The type of the data (used with datatype.DataType)
