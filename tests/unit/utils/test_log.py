c82b04932edb27bdc069c1655d5bfbc063e8aca0
David Afonso <davafons@gmail.com>
2020-01-04 12:30:34 +0000

    :white_check_mark: Add to "test_log". Check log level exception
    Check that when an invalid log level is specified the `init_logs` method throws an exception

--- tests/unit/utils/test_log.py
+++ tests/unit/utils/test_log.py
@@ -19,7 +19,7 @@ def namespace():
     return __namespace
 
 
-def test_init_logs_with_debug(namespace):
+def test_init_with_debug(namespace):
     namespace.debug = True
     log.init_logs(namespace)
 
@@ -29,10 +29,18 @@ def test_init_logs_with_debug(namespace):
 @pytest.mark.parametrize(
     "expected_log_level", ["debug", "info", "warning", "critical", "error"]
 )
-def test_init_logs_with_log_level(namespace, expected_log_level):
+def test_init_with_log_level(namespace, expected_log_level):
     namespace.loglevel = expected_log_level
     log.init_logs(namespace)
 
     assert log.LOG_LEVEL == getattr(logging, expected_log_level.upper())
 
     assert log.get_logger("test").level == log.LOG_LEVEL
+
+
+def test_init_with_invalid_log_level(namespace):
+    namespace.loglevel = "invalid"
+
+    # Setting an invalid log level should throw an exception
+    with pytest.raises(ValueError):
+        log.init_logs(namespace)
