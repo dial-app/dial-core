fa71a8103e3c8fb8d9a4108f547ffa7aecc76937
David Afonso <davafons@gmail.com>
2020-01-04 00:23:56 +0000

    :rotating_light: Removing UPPER_CASE warning on __init__.py modules

--- dial/gui/widgets/datasets_list/__init__.py
+++ dial/gui/widgets/datasets_list/__init__.py
@@ -1,4 +1,5 @@
 # vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
+# pylint: disable=C0103
 
 """
 Widgets for displaying a dialog with predefined datasets that can be loaded.
--- dial/gui/widgets/windows/__init__.py
+++ dial/gui/widgets/windows/__init__.py
@@ -1,4 +1,5 @@
 # vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
+# pylint: disable=C0103
 
 """
 Window widgets displayed on the main window tabs.
