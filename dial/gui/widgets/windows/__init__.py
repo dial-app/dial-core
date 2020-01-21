50e683b51841f76d492a0a9605869f5dd2df1d62
David Afonso <davafons@gmail.com>
2020-01-21 09:31:03 +0000

    :recycle: Moving dependency injection containers to a "containers.py" file
    Cleaner approach I guess

--- dial/gui/widgets/windows/__init__.py
+++ dial/gui/widgets/windows/__init__.py
@@ -4,19 +4,7 @@
 """
 Window widgets displayed on the main window tabs.
 """
-from . import datasets_window, models_window
-from dial.gui.widgets import TrainTestTabs
 
-import dependency_injector.providers as providers
-import dependency_injector.containers as containers
-
-
-class Windows(containers.DeclarativeContainer):
-    datasets = providers.Factory(
-        datasets_window.DatasetsWindow, dataset_table_widget=TrainTestTabs()
-    )
-
-    models = providers.Factory(models_window.ModelsWindow)
-
+from .containers import Windows
 
 __all__ = ["Windows"]
---
+++ dial/gui/widgets/windows/containers.py
@@ -0,0 +1,23 @@
+# vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
+
+"""
+Dependency Injection containers.
+"""
+
+import dependency_injector.containers as containers
+import dependency_injector.providers as providers
+from dial.gui.widgets import TrainTestTabs
+
+from . import datasets_window, models_window
+
+
+class Windows(containers.DeclarativeContainer):
+    """
+    Container for creating window instances.
+    """
+
+    datasets = providers.Factory(
+        datasets_window.DatasetsWindow, dataset_table_widget=TrainTestTabs()
+    )
+
+    models = providers.Factory(models_window.ModelsWindow)
