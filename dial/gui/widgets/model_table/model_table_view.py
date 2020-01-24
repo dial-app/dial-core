4b86336c65bce2f5d873c2cb94410209a1fe5301
David Afonso <davafons@gmail.com>
2020-01-24 11:59:27 +0000

    :lipstick: Model table columns can be shown/hidden using a right click menu

--- dial/gui/widgets/model_table/model_table_view.py
+++ dial/gui/widgets/model_table/model_table_view.py
@@ -3,7 +3,8 @@
 """
 """
 
-from PySide2.QtWidgets import QHeaderView, QTableView
+from PySide2.QtCore import Qt
+from PySide2.QtWidgets import QAction, QHeaderView, QMenu, QTableView
 
 
 class ModelTableView(QTableView):
@@ -13,5 +14,49 @@ class ModelTableView(QTableView):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        # Actions
+        self.__hide_action = QAction("hue", self)
+
         self.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.verticalHeader().setSectionResizeMode(QHeaderView.Interactive)
+
+        self.horizontalHeader().setContextMenuPolicy(Qt.CustomContextMenu)
+        self.horizontalHeader().customContextMenuRequested.connect(
+            self.show_header_context_menu
+        )
+
+        self.__header_context_menu = None
+
+    def setModel(self, model):
+        # Assign model to view by calling the parent method
+        super().setModel(model)
+
+        # Additional: Create a context menu for showing/hiding the table headers
+        # (columns)
+        self.__header_context_menu = QMenu(self)
+        header = self.horizontalHeader()
+
+        # Iterate through each column
+        for i in range(header.count()):
+            column_label = model.headerData(
+                section=i, orientation=Qt.Horizontal, role=Qt.DisplayRole
+            )
+
+            # Add a checkable action corresponding to each column
+            action = self.__header_context_menu.addAction(column_label)
+            action.setCheckable(True)
+            action.setChecked(True)
+
+            # Each action will show/hide the corresponding column referenced by its
+            # index depending on if the action is checked or not (toggled)
+            action.toggled.connect(
+                lambda toggled, index=i: header.showSection(index)
+                if toggled
+                else header.hideSection(index)
+            )
+
+    def show_header_context_menu(self, point):
+        if not self.__header_context_menu:
+            return
+
+        self.__header_context_menu.popup(self.horizontalHeader().mapToGlobal(point))
