77b4c4d7006ed6c41038b54f7c04fc5472d41871
David Afonso <davafons@gmail.com>
2020-01-25 15:57:31 +0000

    :arrow_up: Fixing dependency version locked for "qimage2ndarray"
    Version must be >=1.8, not ==1.8

--- dial/__init__.py
+++ dial/__init__.py
@@ -11,8 +11,8 @@ __description__ = "A Deep Learning GUI-based Framework"
 
 __requirements__ = [
     ("tensorflow", ">=2.0.0a0"),
-    ("PySide2", ">=5.13.1"),
+    ("PySide2", ">=5.12.6"),
     ("Pillow", ">=6.2.1"),
-    ("qimage2ndarray", "==1.8"),
+    ("qimage2ndarray", ">=1.8"),
     ("dependency-injector", ">=3.14.12"),
 ]
