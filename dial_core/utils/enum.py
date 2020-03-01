38ca23279b983f51a2c087501411d5d053d3e7d5
David Afonso <davafons@gmail.com>
2020-03-01 19:55:17 +0000

    :green_heart: Fixing all tests, tox tests and Travis

--- dial_core/utils/enum.py
+++ dial_core/utils/enum.py
@@ -6,5 +6,4 @@ from enum import Enum
 class Dial(Enum):
     """The Dial class contains several identifiers used throughout the application."""
 
-    TypeRole = 20
     KerasLayerListMIME = "application/keras.layer.list"
