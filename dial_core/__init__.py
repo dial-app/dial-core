6f9520f431d1683dbf105a9809c1efa6e9e8862b
David Afonso <davafons@gmail.com>
2020-03-01 23:13:29 +0000

    :wrench: Replacing Pipenv with Poetry

--- dial_core/__init__.py
+++ dial_core/__init__.py
@@ -2,16 +2,6 @@
 
 """A Deep Learning, node-based framework"""
 
-__author__ = "David Afonso"
-__license__ = "GPL"
-__maintainer__ = __author__
-__url__ = "https://github.com/dial-app/dial-core"
-__version__ = "0.5a0"
-__description__ = "A Deep Learning, node-based framework"
-
-__requirements__ = [
-    ("tensorflow", ">=2.0.0a0"),
-    ("dependency-injector", ">=3.14.12"),
-]
+from . import datasets, node_editor, project, utils
 
 __all__ = ["datasets", "node_editor", "project", "utils"]
